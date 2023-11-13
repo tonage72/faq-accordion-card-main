bf5e0ffb11cca5d5dbd46e12f9abaf781542cf33
David Fleming <tonage72@gmail.com>
2023-11-13 18:41:55 -0500

    Need to close other drops downs.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const accordion = document.getElementsByClassName('qa')
 
 for (i = 0; i < accordion.length; i++) {
 	accordion[i].addEventListener('click', function() {
-		this.classList.toggle('active')
+		this.classList.toggle('active');
 	})
 }
