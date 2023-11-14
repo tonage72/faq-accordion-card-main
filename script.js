3458877f5c6cf004c6573203ae2bfb390511ab1d
David Fleming <tonage72@gmail.com>
2023-11-13 19:36:27 -0500

    Need to change bg mobile and change arrows to img

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const accordion = document.getElementsByClassName('qa')
 
 for (i = 0; i < accordion.length; i++) {
 	accordion[i].addEventListener('click', function() {
-		this.classList.toggle('active');
+	
+		if (this.classList.contains('active')) {
+			for (i = 0; i < accordion.length; i++) {
+				accordion[i].classList.remove('active');
+			}
+		} else {
+			for (i = 0; i < accordion.length; i++) {
+				accordion[i].classList.remove('active');
+			}
+			this.classList.add('active');
+		}
 	})
 }
